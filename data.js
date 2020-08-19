e48e8e572f9df5039fdc91c844099e6f1eb5b104
thnamm <nam.tran@journeyh.io>
2020-08-19 16:56:18 +0700

    Apply clean code

---
+++ data.js
@@ -0,0 +1,31 @@
+var data = [{
+    id: 1,
+    linkPicture: "./images/pic1.png",
+    caption: "HASHIRAMA",
+    content: "First Hokage of Leaf Village"
+}, {
+    id: 2,
+    linkPicture: "./images/pic2.png",
+    caption: "TOBIRAMA",
+    content: "Second Hokage of Leaf Village"
+}, {
+    id: 3,
+    linkPicture: "./images/pic3.png",
+    caption: "SARUTOBI",
+    content: "Third Hokage of Leaf Village"
+}, {
+    id: 4,
+    linkPicture: "./images/pic4.png",
+    caption: "MINATO",
+    content: "Fourth Hokage of Leaf Village"
+}, {
+    id: 5,
+    linkPicture: "./images/pic5.png",
+    caption: "TSUNADE",
+    content: "Fifth Hokage of Leaf Village"
+}, {
+    id: 6,
+    linkPicture: "./images/pic6.png",
+    caption: "KAKASHI",
+    content: "Sixth Hokage of Leaf Village"
+}];
